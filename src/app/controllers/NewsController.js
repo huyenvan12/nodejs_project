5abfb5addf7793e35cf67f0114cef19523a92202
huyenvan12 <wendy.hngoc@gmail.com>
2023-12-07 10:46:53 +0700

    Refactor code: create routes, app/controllers folders

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -3,6 +3,10 @@ class NewsController {
     index(req, res) {
         res.render('news');
     }
+    //[GET] /news/:slug
+    show(req, res) {
+        res.send('NEWS DETAILS!!!')
+    }
 }
 
 module.exports = new NewsController
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const hbs = require('express-handlebars')
 const app = express()
 const port = 3000
 const path = require('path')
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, "public")))
 
-// app.use(morgan('combined'))
+// HTTP logger
+app.use(morgan('combined'))
 
 app.use(express.urlencoded({
     extended: true
@@ -21,19 +23,8 @@ app.engine('hbs', hbs.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/resources/views')
 
-//Local host --- Hosting
-
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/search', (req, res) => {
-    res.render('search');
-})
-app.post('/search', (req, res) => {
-    console.log(req.body);
-    res.send(' ');
-})
+//Route init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
